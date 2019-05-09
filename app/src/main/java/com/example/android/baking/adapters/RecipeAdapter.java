10c3df3eadadb748baf927ba40490ed11db410e1
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-05-09 14:35:00 -0700

    laying out basic structure

---
+++ app/src/main/java/com/example/android/baking/adapters/RecipeAdapter.java
@@ -0,0 +1,4 @@
+package com.example.android.baking.adapters;
+
+public class RecipeAdapter {
+}
---
+++ app/src/main/java/com/example/android/baking/fragments/RecipeDetailFragment.java
@@ -0,0 +1,4 @@
+package com.example.android.baking.fragments;
+
+public class RecipeDetailFragment {
+}
